73e721e79bf89e50cdf1071a14e995328163d64c
William Gross <william@wgross.net>
2024-05-15 10:29:34 -0400

    Renamed ActionTools to ExceptionHandlingTools
    Preparation for next commit

---
+++ Tewl/Tools/ExceptionHandlingTools.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading;
+using JetBrains.Annotations;
+
+namespace Tewl.Tools;
+
+/// <summary>
+/// Static methods pertaining to exception handling.
+/// </summary>
+[ PublicAPI ]
+public static class ExceptionHandlingTools {
+	/// <summary>
+	/// Retries the given action until it executes without exception or maxAttempts is reached. You can specify different
+	/// maxAttempts or retry intervals - the default is 30 tries with a 2-second wait in between each try.
+	/// If every attempt fails, a new application exception will be thrown with the given message. The original exception will
+	/// be the inner exception.
+	/// </summary>
+	public static void Retry( Action action, string failureMessage, int maxAttempts = 30, int retryIntervalMs = 2000 ) {
+		for( var i = 0;; i += 1 )
+			try {
+				action();
+				break;
+			}
+			catch( Exception e ) {
+				if( i < maxAttempts )
+					Thread.Sleep( retryIntervalMs );
+				else
+					throw new ApplicationException( failureMessage, e );
+			}
+	}
+}
