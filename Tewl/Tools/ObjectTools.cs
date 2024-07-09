f11e122d9d66cd080108f3aac1ca047f542f33ad
William Gross <william@wgross.net>
2024-07-09 15:29:09 -0400

    Added nullability annotations to ObjectToString

--- Tewl/Tools/ObjectTools.cs
+++ Tewl/Tools/ObjectTools.cs
@@ -1,19 +1,15 @@
 ﻿using JetBrains.Annotations;
 
-namespace Tewl.Tools {
+namespace Tewl.Tools;
+
+/// <summary>
+/// Extension methods and other static methods pertaining to things of type object.
+/// </summary>
+[ PublicAPI ]
+public static class ObjectTools {
 	/// <summary>
-	/// Extension methods and other static methods pertaining to things of type object.
+	/// Returns o.ToString() unless o is null. In this case, returns either null (if nullToEmptyString is false) or the empty
+	/// string (if nullToEmptyString is true).
 	/// </summary>
-	[ PublicAPI ]
-	public static class ObjectTools {
-		/// <summary>
-		/// Returns o.ToString() unless o is null. In this case, returns either null (if nullToEmptyString is false) or the empty
-		/// string (if nullToEmptyString is true).
-		/// </summary>
-		public static string ObjectToString( this object o, bool nullToEmptyString ) {
-			if( o != null )
-				return o.ToString();
-			return nullToEmptyString ? string.Empty : null;
-		}
-	}
+	public static string? ObjectToString( this object? o, bool nullToEmptyString ) => o is not null ? o.ToString() : nullToEmptyString ? string.Empty : null;
 }
